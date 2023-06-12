3e1c5630ec6fc0bff16cd9a4b77a0696d4169416
Amitha95 <99869312+Amitha95@users.noreply.github.com>
2023-06-12 12:35:10 +0530

    Update urls.py

--- club/urls.py
+++ club/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("",views.index,name="index"),
     path("index",views.index,name="index"),
@@ -8,3 +9,5 @@ urlpatterns = [
     path("services",views.services,name="services"),
     path("contact",views.contact,name="contact"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
